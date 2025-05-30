dc4a0ae73b5c3ba15d541d50bd319a9b2f75d04c
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2025-05-30 14:17:00 +0300

    Switch to gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,7 @@ export default defineConfig({
   plugins: [react()],
   base: '/myportfolio/',
   build: {
-    sourcemap: true,
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+    outDir: 'dist'
   },
   server: {
     open: true,
